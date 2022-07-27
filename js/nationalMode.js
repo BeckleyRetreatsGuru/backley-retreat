8d796fe86b470c8d4c1800a6a216bdf8f5590c34
BeckleyRetreatsGuru <110103683+BeckleyRetreatsGuru@users.noreply.github.com>
2022-07-27 15:23:20 +0300

    Create nationalMode.js

---
+++ js/nationalMode.js
@@ -0,0 +1,33 @@
+async function callFuncAfterTimeOut(miliSec, callFunction) {
+    await new Promise(resolve => setTimeout(resolve, miliSec));
+    return callFunction()
+}
+
+async function initializeItiPlaceholder() {
+    let input = document.querySelector("#international_PhoneNumber_countrycode")
+
+    let iti = await window.intlTelInput(input, {
+        nationalMode: true,
+        initialCountry: window.country ?? "US",
+        customPlaceholder:  await function (selectedCountryPlaceholder, selectedCountryData) {
+            console.log('window.country: ', window.country)
+            let returnData = '';
+            console.log("selectedCountryPlaceholder: ", selectedCountryPlaceholder)
+            console.log("selectedCountryData: ", selectedCountryData)
+            let mask = selectedCountryPlaceholder
+                .replace(/[0-9]/g, 'x')
+                .replace(/[()-]/g, ' ')
+            mask = selectedCountryData.dialCode + " " + mask
+            console.log("mask: ", mask)
+            $("#international_PhoneNumber_countrycode").val('');
+            $("#international_PhoneNumber_countrycode").mask("+" + mask);
+            returnData = "+" + selectedCountryData.dialCode + " " + selectedCountryPlaceholder;
+            return returnData;
+        },
+        utilsScript: "https://dimakhomiak.github.io/backley-retreat/js/utils.js?1638200991544", // just for formatting/placeholders etc
+    });
+}
+
+
+callFuncAfterTimeOut(1000, initializeItiPlaceholder).then()
+
